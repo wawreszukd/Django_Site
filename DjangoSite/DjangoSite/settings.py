b4684a1ac47e8eb89c9eff537fc0f8894df535ac
wawreszukd <wawreszukd@gmail.com>
2023-01-14 14:34:14 +0100

    Static files fix

--- DjangoSite/DjangoSite/settings.py
+++ DjangoSite/DjangoSite/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
 MEDIA_URL = 'images/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
